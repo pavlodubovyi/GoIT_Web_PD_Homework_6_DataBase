1129703e4b57de065078f283699957c5d6970145
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2024-04-02 22:22:14 +0200

    9
    9

--- query_8.sql
+++ query_8.sql
@@ -1,3 +1,14 @@
 -- Знайти середній бал, який ставить певний викладач зі своїх предметів
 -- 1) Run the following script in SQL console for test database OR
 -- 2) Copy-Paste the following script in "select.py" after "sql_expression_from_query = """
+
+SELECT
+    teachers.id,
+    teachers.fullname,
+    subjects.name,
+    ROUND(AVG(grades.grade), 2)
+FROM grades
+JOIN subjects ON grades.subject_id = subjects.id
+JOIN teachers ON subjects.teacher_id = teachers.id
+WHERE teachers.id = 1
+GROUP BY teachers.id, teachers.fullname, subjects.name;
--- query_9.sql
+++ query_9.sql
@@ -1,3 +1,16 @@
 -- Знайти список курсів, які відвідує студент
 -- 1) Run the following script in SQL console for test database OR
 -- 2) Copy-Paste the following script in "select.py" after "sql_expression_from_query = """
+
+SELECT
+    students.id,
+    students.fullname,
+    subjects.name
+FROM grades
+JOIN students ON grades.student_id = students.id
+JOIN subjects ON grades.subject_id = subjects.id
+WHERE students.id = 1 -- enter desired student id
+GROUP BY
+    subjects.name,
+    students.fullname,
+    students.id;
